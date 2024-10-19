5118192e6e8e9ff451b1ef2facce8075e1b0f2e0
Rayhan Alfa Rezki <125686480+rayhanrezki@users.noreply.github.com>
2024-10-20 00:11:44 +0700

    Delete app directory

--- app/views/about/index.php
+++
@@ -1,3 +0,0 @@
-
-    <h1>About Me</h1>
-    <p> Halo nama saya adalah <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun saya merupakan seorang <?= $data['pekerjaan']; ?> yang sedang belajar praktikum website</p>
--- app/views/home/index.php
+++
@@ -1,2 +0,0 @@
-
-  <h1>Selamat datang di website saya</h1>
