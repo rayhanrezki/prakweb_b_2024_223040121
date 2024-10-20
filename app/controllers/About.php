bb006eef508a6df25ffa38673ddf64a2f097318a
rayhanrezki <rayhanalfarezki@gmail.com>
2024-10-20 07:49:23 +0700

    video 4  mvc

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php 
+class About 
+{
+  public function index($nama = 'alfa', $pekerjaan ='mahasiswa'  )
+  {
+    echo "Halo nama saya $nama, saya adalah seorang $pekerjaan ";
+  }
+  public function page (){
+    echo 'about/page';
+  }
+}
+?>
